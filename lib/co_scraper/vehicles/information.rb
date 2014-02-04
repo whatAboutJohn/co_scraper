cf0ec30e378d32a41308add74ed2554d2b3ce249
John Molina <john.molinam@hotmail.com>
2014-02-03 21:10:13 -0400

    Add lib folder. Add information.rb.

---
+++ lib/co_scraper/vehicles/information.rb
@@ -0,0 +1,22 @@
+#############################################||
+
+####### Clasificados Online Scraper #########||
+
+########## Author: John Molina ##############||
+
+####### Email: john@pixelhipsters.com #######||
+
+#############################################||
+
+# information.rb: Scrape all of the available 
+# vehicles listed.
+
+module CoScraper
+
+  def self.included(base)
+    base.extend self
+  end
+
+  
+
+end
